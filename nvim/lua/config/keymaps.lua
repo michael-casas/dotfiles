ee125049b2fa7579083a4af0bc865492bba5df3e
Michael Casas <deploy@aes-mlb.com>
2026-04-23 01:14:02 -0400

    feat(nvim): Option bracket buffer cycling
    Map M-[ and M-] to cycle through buffers in Normal mode.
    These keys are not claimed by tmux, so they pass through cleanly.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Buffer cycling with Option+[ and Option+]
+-- Note: M-[ can conflict with terminal escape sequences on some terminals.
+-- If you experience issues, switch to M-( and M-) or M-p/M-n.
+vim.keymap.set("n", "<M-[>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<M-]>", "<cmd>bnext<cr>", { desc = "Next buffer" })
